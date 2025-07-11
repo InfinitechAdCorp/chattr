dc7665c3d415a52b094f33b6cda4e7b24e70e65a
Jmdec <infinitech.justin2024@gmail.com>
2025-07-11 09:35:19 +0800

    pwa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // disable PWA in dev
+  disable: process.env.NODE_ENV === 'production', // disable PWA in dev
 });
 
 const nextConfig = {
